1576854fd5831daaff33cee0bb7a9c5eb4fc6a3d
Kilian von Pflugk <kilian.vonpflugk@innovis-kg.de>
2024-03-06 15:46:39 +0100

    release

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM oven/bun:1.0.30-alpine
+
+WORKDIR /relay
+COPY . .
+ADD https://github.com/Yelp/dumb-init/releases/download/v1.2.5/dumb-init_1.2.5_x86_64 /bin/dumb-init
+RUN bun install --production --ignore-scripts &&\
+    chmod +x /bin/dumb-init
+ENTRYPOINT ["dumb-init", "--"]
+CMD ["bun", "app.ts"]
+
+EXPOSE 25
---
+++ app.ts
@@ -0,0 +1,47 @@
+import { SMTPServer } from 'smtp-server';
+import nodemailer from "nodemailer";
+import { ParsedMail, simpleParser } from "mailparser";
+
+const server = new SMTPServer(
+  {
+    onData(stream, _Session, callback) {
+      simpleParser(stream, {}, (err, parsed) => {
+        if (err) {
+          console.log("Error:", err);
+        } else {
+          sendEmail(parsed);
+          stream.on("end", callback);
+        }
+      });
+      return callback();
+    },
+    disabledCommands: ["STARTTLS", "AUTH"],
+  });
+server.listen(25);
+
+async function sendEmail(email: ParsedMail) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.office365.com",
+    port: 587,
+    secure: false,
+    tls: {
+      maxVersion: 'TLSv1.3',
+      minVersion: 'TLSv1.2',
+    },
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS,
+    },
+  });
+
+  transporter.sendMail({
+    from: process.env.FROM,
+    to: process.env.TO,
+    subject: email.subject,
+    text: email.text,
+    html: email.textAsHtml,
+  }, (_err, info) => {
+    console.log(info.envelope);
+    console.log(info.messageId);
+  });
+}
