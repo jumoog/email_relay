700bf9315347f089bad08da5202528a63128379d
Kilian von Pflugk <github@jumoog.io>
2024-08-25 22:50:30 +0200

    code refactoring

--- app.ts
+++ app.ts
@@ -1,47 +1,53 @@
 import { SMTPServer } from 'smtp-server';
-import nodemailer from "nodemailer";
-import { ParsedMail, simpleParser } from "mailparser";
+import nodemailer from 'nodemailer';
+import { simpleParser, ParsedMail } from 'mailparser';
 
-const server = new SMTPServer(
-  {
-    onData(stream, _Session, callback) {
-      simpleParser(stream, {}, (err, parsed) => {
-        if (err) {
-          console.log("Error:", err);
-        } else {
-          sendEmail(parsed);
-          stream.on("end", callback);
-        }
+const server = new SMTPServer({
+  onData(stream, _session, callback) {
+    simpleParser(stream)
+      .then((parsed) => sendEmail(parsed))
+      .catch((err) => {
+        console.error("Parsing error:", err);
+        callback(err);
+      })
+      .finally(() => {
+        stream.on('end', callback);
       });
-      return callback();
-    },
-    disabledCommands: ["STARTTLS", "AUTH"],
-  });
-server.listen(25);
+  },
+  disabledCommands: ['STARTTLS', 'AUTH'],
+});
+
+server.listen(25, () => {
+  console.log('SMTP server is listening on port 25');
+});
 
 async function sendEmail(email: ParsedMail) {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false,
-    tls: {
-      maxVersion: 'TLSv1.3',
-      minVersion: 'TLSv1.2',
-    },
-    auth: {
-      user: process.env.USER,
-      pass: process.env.PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.office365.com',
+      port: 587,
+      secure: false,
+      tls: {
+        maxVersion: 'TLSv1.3',
+        minVersion: 'TLSv1.2',
+      },
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+    });
 
-  transporter.sendMail({
-    from: process.env.FROM,
-    to: process.env.TO,
-    subject: email.subject,
-    text: email.text,
-    html: email.textAsHtml,
-  }, (_err, info) => {
-    console.log(info.envelope);
-    console.log(info.messageId);
-  });
+    const info = await transporter.sendMail({
+      from: process.env.FROM,
+      to: process.env.TO,
+      subject: email.subject,
+      text: email.text,
+      html: email.html || email.textAsHtml, // html property has more priority if available
+    });
+
+    console.log('Email sent:', info.messageId);
+    console.log('Envelope:', info.envelope);
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
 }
